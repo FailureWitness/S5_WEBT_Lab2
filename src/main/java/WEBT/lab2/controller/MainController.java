9abee75f4048f6c9ce62ea059e384c1db5d44168
FailureWitness <evgeny@vasyliev.com>
2020-11-08 18:54:42 +0200

    Spring MVC initialisation

---
+++ src/main/java/WEBT/lab2/controller/MainController.java
@@ -0,0 +1,13 @@
+package WEBT.lab2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	private String home_Get() {
+		return "home";
+	}
+}
