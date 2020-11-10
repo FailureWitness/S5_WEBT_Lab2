0304b92fe01d76f0b2ba190665900391eccbd3ce
FailureWitness <evgeny@vasyliev.com>
2020-11-11 01:05:56 +0200

    Drag'n'drop added

--- src/main/webapp/resources/js/home.js
+++ src/main/webapp/resources/js/home.js
@@ -2,7 +2,59 @@ const infoPopup = document.getElementById("info-popup")
 const list = document.getElementById("list")
 const form = document.getElementById("create-form")
 const localStorageKey = "S5_WEBT_Lab2-list"
+let draggingElement = null
 
+const objectFromNode = function(node){
+	text = node.querySelector(".list-item-text")
+	return {
+		text: text.innerHTML,
+		done: text.classList.contains("done")
+	}
+}
+const saveListToLocalStorage = function(){
+	nodeList = list.querySelectorAll(".list-item")
+	resultArr = []
+	for(i = 0; i < nodeList.length - 1; i++){
+		resultArr.push(objectFromNode(nodeList[i]))
+	}
+	localStorage.setItem(localStorageKey, JSON.stringify(resultArr))
+}
+
+const onDragStart = function(e){
+	draggingElement = this
+	setTimeout(() => this.style.display = "none", 0)
+}
+const onDragEnter = function(e){
+	this.querySelector(".drop-place").style.display = "block"
+}
+const onDragOver = function(e){
+	e.preventDefault()
+}
+// Using to prevent a bug (at start upper child shows 'drop place' when dragging down by list)
+const onDragOverErr = function(e){
+	this.parentNode.querySelector(".drop-place").style.display = "none"
+}
+const onDragLeave = function(e){
+	this.style.display = "none"
+}
+const onDragEnd = function(e){
+	this.style.display = "block"
+}
+const onDragDrop = function(e){
+	list.insertBefore(draggingElement, this.parentNode)
+	this.style.display = "none"
+	draggingElement = null
+	saveListToLocalStorage()
+}
+
+form.parentNode.addEventListener("dragenter", onDragEnter)
+form.parentNode.querySelector(".drop-place")
+	.addEventListener("dragleave", onDragLeave)
+form.parentNode.querySelector(".drop-place")
+	.addEventListener("dragover", onDragOver)
+form.parentNode.querySelector(".drop-place")
+	.addEventListener("drop", onDragDrop)
+	
 const done = function(){
 	this.parentNode
 		.querySelector(".list-item-text")
@@ -22,10 +74,11 @@ const undo = function(){
 	saveListToLocalStorage()
 }
 const removeItem = function(){
-	this.parentNode.remove()
+	this.parentNode.parentNode.remove()
 	saveListToLocalStorage()
 }
-const nodeFromObject = function(item){
+
+const nodeFromObject = function(item){	
 	text = document.createElement("p")
 	text.classList.add("list-item-text")
 	text.innerHTML = item.text
@@ -42,8 +95,7 @@ const nodeFromObject = function(item){
 	}
 		
 	removeButton = document.createElement("button")
-	removeButton.classList.add("danger")
-	removeButton.classList.add("list-item-button")
+	removeButton.classList.add("danger", "list-item-button")
 	removeButton.onclick = removeItem
 	removeButton.innerHTML = "remove"
 	
@@ -52,12 +104,30 @@ const nodeFromObject = function(item){
 	listItem.appendChild(text)
 	listItem.appendChild(doneButton)
 	listItem.appendChild(removeButton)
-	listItem.draggable = true
 	
-	return listItem
+	listItem.addEventListener("dragover", onDragOverErr) // Using to prevent a bug (at start upper child shows 'drop place' when dragging down by list)
+	
+	dropPlace = document.createElement("div")
+	dropPlace.classList.add("drop-place")
+	
+	dropPlace.addEventListener("dragleave", onDragLeave)
+	dropPlace.addEventListener("drop", onDragDrop)
+	dropPlace.addEventListener("dragover", onDragOver)
+	
+	jsListItemWrapper = document.createElement("div")
+	jsListItemWrapper.classList.add("js-list-item-wrapper")
+	jsListItemWrapper.appendChild(dropPlace)
+	jsListItemWrapper.appendChild(listItem)
+	jsListItemWrapper.draggable = true
+	
+	jsListItemWrapper.addEventListener("dragenter", onDragEnter)
+	jsListItemWrapper.addEventListener("dragstart", onDragStart)
+	jsListItemWrapper.addEventListener("dragend", onDragEnd)
+	
+	return jsListItemWrapper
 }
 const appendItem = function(item){
-	list.insertBefore(nodeFromObject(item), form)
+	list.insertBefore(nodeFromObject(item), form.parentNode)
 }
 const readListFromLocalStorage = function(){
 	jsonArr = localStorage.getItem(localStorageKey)
@@ -68,7 +138,7 @@ const readListFromLocalStorage = function(){
 	}
 }
 const refreshList = function(arr){
-	nodeList = list.querySelectorAll(".list-item")
+	nodeList = list.querySelectorAll(".js-list-item-wrapper")
 	for(i = 0; i < nodeList.length - 1; i++){
 		nodeList[i].remove()
 	}
@@ -77,22 +147,6 @@ const refreshList = function(arr){
 	})
 }
 refreshList(readListFromLocalStorage())
-
-const objectFromNode = function(node){
-	text = node.querySelector(".list-item-text")
-	return {
-		text: text.innerHTML,
-		done: text.classList.contains("done")
-	}
-}
-const saveListToLocalStorage = function(){
-	nodeList = list.querySelectorAll(".list-item")
-	resultArr = []
-	for(i = 0; i < nodeList.length - 1; i++){
-		resultArr.push(objectFromNode(nodeList[i]))
-	}
-	localStorage.setItem(localStorageKey, JSON.stringify(resultArr))
-}
 
 const showInfo = function(){
 	infoPopup.style.display = "block"
@@ -121,7 +175,9 @@ form.onsubmit = e => {
 	return false;
 }
 
-setInterval(()=>{
-		refreshList(readListFromLocalStorage())
-	}, 3000)
+//setInterval(()=>{
+//	refreshList(readListFromLocalStorage())
+//}, 3000)
+
+
 // Made by EugeneVV
